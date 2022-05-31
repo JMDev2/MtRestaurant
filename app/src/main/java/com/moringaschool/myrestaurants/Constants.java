002d77ae993c7a2e289e5033cd4e93bb3141330c
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-31 17:52:46 +0300

    Tghis is a complete app with YELP API

---
+++ app/src/main/java/com/moringaschool/myrestaurants/Constants.java
@@ -0,0 +1,10 @@
+package com.moringaschool.myrestaurants;
+
+import com.moringaschool.myrestaurants.BuildConfig;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+//    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+
+}
---
+++ app/src/main/java/com/moringaschool/myrestaurants/models/Region.java
@@ -0,0 +1,42 @@
+
+package com.moringaschool.myrestaurants.models;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.moringaschool.myrestaurants.models.Center;
+
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class Region {
+
+    @SerializedName("center")
+    @Expose
+    private Center center;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Region() {
+    }
+
+    /**
+     * 
+     * @param center
+     */
+    public Region(Center center) {
+        super();
+        this.center = center;
+    }
+
+    public Center getCenter() {
+        return center;
+    }
+
+    public void setCenter(Center center) {
+        this.center = center;
+    }
+
+}
